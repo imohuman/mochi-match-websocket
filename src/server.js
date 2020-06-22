e0f87a6e337f983ab25653c1ab19e9c13134bc89
taniwhy <>
2020-06-22 09:25:31 +0900

    redis接続をコメント化で無効

--- src/server.js
+++ src/server.js
@@ -1,53 +1,46 @@
-const socketio = require("socket.io");
-const redis = require("redis");
-const fs = require("fs");
-
-const server = require("http").createServer();
-
+const socketio = require('socket.io');
+const server = require('http').createServer();
 const io = socketio(server);
-io.set("heartbeat interval", 5000);
-io.set("heartbeat timeout", 15000);
+io.set('heartbeat interval', 5000);
+io.set('heartbeat timeout', 15000);
+//const redis = require('redis');
+//const sub = redis.createClient(
+//  Number(process.env.REDIS_PORT) || 6379,
+//  process.env.REDIS_HOST || 'localhost'
+//);
+//sub.subscribe('channel_1');
 
-const sub = redis.createClient(
-  Number(process.env.REDIS_PORT) || 6379,
-  process.env.REDIS_HOST || "localhost"
-);
-sub.subscribe("channel_1");
+var user_name = '';
+var room_id = '';
 
-var user_name = "";
-var room_id = "";
-
-sub.on("message", function (channel, message) {
-  var m = JSON.parse(message);
-  console.log(m);
-  chat.to(room_id).emit("msg", message);
-});
-const chat = io.of("chatroom");
-chat.on("connection", function (socket) {
-  socket.on("join_req", function (data) {
-    console.log("join");
-    console.log(data);
+//sub.on('message', function (channel, message) {
+//  var m = JSON.parse(message);
+//  chat.to(room_id).emit('msg', m);
+//});
+const chat = io.of('chatroom');
+chat.on('connection', function (socket) {
+  socket.on('join_req', function (data) {
     user_name = data.user.user_name;
     room_id = data.room_id;
     socket.join(room_id);
-    socket.broadcast.to(room_id).emit("notify_entry", data);
+    socket.broadcast.to(room_id).emit('notify_entry', data);
   });
 
-  socket.on("start_input", function (data) {
-    console.log("input start");
+  socket.on('start_input', function (data) {
+    console.log('input start');
     socket.broadcast
       .to(room_id)
-      .emit("user_input_start", { user_name: user_name });
+      .emit('user_input_start', { user_name: user_name });
   });
 
-  socket.on("stop_input", function (data) {
-    console.log("stop input");
+  socket.on('stop_input', function (data) {
+    console.log('stop input');
     socket.broadcast
       .to(room_id)
-      .emit("user_input_stop", { user_name: user_name });
+      .emit('user_input_stop', { user_name: user_name });
   });
 
-  socket.on("disconnect", function (data) {
+  socket.on('disconnect', function (data) {
     socket.leave(room_id);
     console.log(data);
     // todo
